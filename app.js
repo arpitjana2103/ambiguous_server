f3d2f1252d8501e5b7f6e0101a753225fe13a0fd
Arpit Jana <arpitjana2103@gmail.com>
2023-08-26 22:22:34 +0530

    Little Update

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', function (req, res) {
+    res.status(200).json({
+        status: 'Success',
+        message: 'Wellcome to GlamGuru',
+    });
+});
+
 app.use('/api/v1/appointments', appointmentRouter);
 app.use('/api/v1/experts', expertRouter);
 app.use('/api/v1/users', userRouter);
